b36a7e6f09b7cf17ea62f1d94e148e9306716551
omega-800 <gshevoroshkin@gmail.com>
2024-05-27 20:09:31 +0200

    nixos forcing me to commit again

--- sys/wm/qtile/qtile.nix
+++ sys/wm/qtile/qtile.nix
@@ -1,8 +1,12 @@
-{ userSettings, ... }: {
+{ userSettings, python3Packages, ... }: {
   imports = [ ../x11/x11.nix ];
   services.xserver.windowManager.qtile = {
     enable = true;
     configFile = "/home/${userSettings.username}/.config/qtile/config.py";
     backend = "x11";
+    extraPackages = with python3Packages; [ 
+      yaml 
+      qtile-extras
+    ];
   };
 }
