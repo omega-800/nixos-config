e5274e7d8ad82b10abec76f684521306bcb5815a
gs2 <georgiy.shevoroshkin@inteco.ch>
2024-06-16 13:25:26 +0200

    uiiuuuiiiuu

---
+++ hosts/z/conf.nix
@@ -0,0 +1,16 @@
+{
+    sys = {
+        bootMode = "uefi"; # uefi or bios
+        genericLinux = false;
+        hardened = true;
+        paranoid = false;
+    };
+    usr = rec {
+        username = "omega"; # username
+        homeDir = "/home/${username}";
+        # profile
+        devName = "gs2";
+        devEmail = "georgiy.shevoroshkin@inteco.ch"; 
+        wm = "dwm"; # Selected window manager or desktop environment; must select one in both ./user/wm/ and ./system/wm/
+    };
+}
