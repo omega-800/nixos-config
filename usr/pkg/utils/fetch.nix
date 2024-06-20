459dcf668f6e1d7124aa971c7ddda05aa77dcd3b
gs2 <georgiy.shevoroshkin@inteco.ch>
2024-06-20 20:54:04 +0200

    asdf

--- usr/pkg/utils/fetch.nix
+++ usr/pkg/utils/fetch.nix
@@ -3,7 +3,6 @@ with lib;
 let cfg = config.u.utils.fetch;
 in {
   options.u.utils.fetch.enable = mkOption {
-      name = "enables essential packages";
       type = types.bool;
       default = config.u.utils.enable;
   };
