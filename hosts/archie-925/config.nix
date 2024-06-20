f893f93fa1cf8bf5c05ec8f15454b28318fbe7e9
asdf <asdf>
2024-06-20 21:44:38 +0000

    asdf

---
+++ hosts/archie-925/config.nix
@@ -0,0 +1,15 @@
+{ ... }: {
+  config.c = {
+    sys = {
+      hostname = "archie-925";
+      profile = "work";
+      system = "x86_64-linux";
+      genericLinux = true;
+    };
+    usr = {
+      devName = "gs2";
+      devEmail = "georgiy.shevoroshkin@inteco.ch";
+      wm = "dwm";
+    };
+  };
+}
