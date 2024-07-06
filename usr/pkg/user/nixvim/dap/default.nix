43fc2e72550a6129506d35656a6dcc0c2c7af188
omega-800 <gshevoroshkin@gmail.com>
2024-07-06 23:26:19 +0200

    moar nvim debugging

--- usr/pkg/user/nixvim/dap/default.nix
+++ usr/pkg/user/nixvim/dap/default.nix
@@ -1,10 +1,22 @@
+{ pkgs, ... }:
+let
+  nvim-dap-vscode-js = pkgs.vimUtils.buildVimPlugin {
+    name = "vim-dap-vscode-js";
+    src = pkgs.fetchFromGitHub {
+      owner = "mxsdev";
+      repo = "nvim-dap-vscode-js";
+      rev = "e7c05495934a658c8aa10afd995dacd796f76091";
+      sha256 = "sha256-lZABpKpztX3NpuN4Y4+E8bvJZVV5ka7h8x9vL4r9Pjk=";
+    };
+  };
+in
 {
   programs.nixvim = {
     keymaps = [
       {
         mode = "n";
-        key = "<leader>dx"; # "<C-F11>"
-        action = ''<cmd>lua require("dap").terminate()<cr>'';
+        key = "<leader>dx";
+        action = "<cmd>DapTerminate<cr>";
         options = { silent = true; };
       }
       {
@@ -29,31 +41,31 @@
       {
         mode = "n";
         key = "<leader>db";
-        action = ''<cmd>lua require("dap").toggle_breakpoint()<cr>'';
+        action = "<cmd>DapToggleBreakpoint<cr>";
         options = { silent = true; };
       }
       {
         mode = "n";
         key = "<leader>dc";
-        action = ''<cmd>lua require("dap").continue()<cr>'';
+        action = "<cmd>DapContinue<cr>";
         options = { silent = true; };
       }
       {
         mode = "n";
         key = "<leader>dn";
-        action = ''<cmd>lua require("dap").step_over()<cr>'';
+        action = "<cmd>DapStepOver<cr>";
         options = { silent = true; };
       }
       {
         mode = "n";
         key = "<leader>do";
-        action = ''<cmd>lua require("dap").step_out()<cr>'';
+        action = "<cmd>DapStepOver<cr>";
         options = { silent = true; };
       }
       {
         mode = "n";
         key = "<leader>di";
-        action = ''<cmd>lua require("dap").step_into()<cr>'';
+        action = "<cmd>DapStepInto<cr>";
         options = { silent = true; };
       }
       {
@@ -94,6 +106,35 @@
           <cmd>lua require("dapui").toggle(1)<cr><cmd>lua require("dapui").toggle(2)<cr>'';
         options = { silent = true; };
       }
+      {
+        mode = "n";
+        key = "<leader>dv";
+        action = "+virtual-text";
+      }
+      {
+        mode = "n";
+        key = "<leader>dvt";
+        action = "<cmd>DapVirtualTextToggle<cr>";
+        options = { silent = true; };
+      }
+      {
+        mode = "n";
+        key = "<leader>dve";
+        action = "<cmd>DapVirtualTextEnable<cr>";
+        options = { silent = true; };
+      }
+      {
+        mode = "n";
+        key = "<leader>dvd";
+        action = "<cmd>DapVirtualTextDisable<cr>";
+        options = { silent = true; };
+      }
+      {
+        mode = "n";
+        key = "<leader>dvr";
+        action = "<cmd>DapVirtualTextForceRefresh<cr>";
+        options = { silent = true; };
+      }
     ];
     plugins.dap = {
       enable = true;
@@ -103,7 +144,113 @@
         dap-go.enable = true;
         dap-python.enable = true;
       };
-      adapters = { };
+      adapters = {
+        executables = { "pwa-node" = "node"; };
+        servers = { "pwa-node" = { }; };
+      };
+      configurations = {
+        "typescript" = [{
+          type = "pwa-node";
+          request = "launch";
+          name = "Launch file";
+          program = "\${file}";
+          cwd = "\${workspaceFolder}";
+        }];
+        "javascript" = [{
+          type = "pwa-node";
+          request = "launch";
+          name = "Launch file";
+          program = "\${file}";
+          cwd = "\${workspaceFolder}";
+        }];
+      };
     };
+    extraPlugins = [ pkgs.vimPlugins.nvim-gdb nvim-dap-vscode-js ];
+    extraConfigLua =
+      # lua
+      ''
+          local dap, dapui = require("dap"), require("dapui")
+          local dap_vscode_js = require("dap-vscode-js")
+          -- DEBUG LISTENERS
+          dap.listeners.before.attach.dapui_config = function()
+          	dapui.open()
+          end
+          dap.listeners.before.launch.dapui_config = function()
+          	dapui.open()
+          end
+          dap.listeners.before.event_terminated.dapui_config = function()
+          	dapui.close()
+          end
+          dap.listeners.before.event_exited.dapui_config = function()
+          	dapui.close()
+          end
+
+          local dap = require('dap')
+          dap.set_log_level('DEBUG')
+
+          -- DEBUG VS CODE
+          dap_vscode_js.setup({
+            adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }
+          })
+
+          -- DEBUG CONFIG TYPESCRIPT
+          dap.configurations.typescript = {
+            {
+              type = "pwa-node",
+              request = "launch",
+              name = "Launch file",
+              program = "''${file}",
+              cwd = "''${workspaceFolder}",
+            },
+            {
+              type = "pwa-node",
+              request = "attach",
+              name = "Attach",
+              processId = require'dap.utils'.pick_process,
+              cwd = "''${workspaceFolder}",
+            },
+            {
+              type = "pwa-node",
+              request = "launch",
+              name = "Run Application",
+              program = "dist/index.js",
+              cwd = "''${workspaceFolder}",
+            },
+            {
+              type = "pwa-node",
+              request = "launch",
+              name = "Run npm test",
+              program = "node_modules/mocha/bin/_mocha",
+              cwd = "''${workspaceFolder}",
+            }
+          }
+
+          -- DEBUG C
+          dap.adapters.lldb = {
+              type = 'executable',
+              command = '${pkgs.lldb_17}/bin/lldb-vscode', -- adjust as needed, must be absolute path
+              name = 'lldb'
+          }
+
+          local dap = require("dap")
+          dap.adapters.gdb = {
+              type = "executable",
+              command = "gdb",
+              args = { "-i", "dap" }
+          }
+
+          local dap = require("dap")
+          dap.configurations.c = {
+        	{
+        		name = "Launch",
+        		type = "gdb",
+        		request = "launch",
+        		program = function()
+        			return vim.fn.input('Path of the executable: ', vim.fn.getcwd() .. '/', 'file')
+        		end,
+        		cwd = "''${workspaceFolder}",
+        	},
+          }
+      '';
   };
 }
