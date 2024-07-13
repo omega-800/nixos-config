3aeb766f8f4cdb911a305fe8d56f6b1104e9044b
asdf <asdf>
2024-07-13 15:48:38 +0200

    asdf

--- usr/pkg/dev/git.nix
+++ usr/pkg/dev/git.nix
@@ -7,34 +7,35 @@ in {
     default = config.u.dev.git.enable;
   };
 
-  home.packages = with pkgs; [
-    git-secrets
-  ];
-  programs.git = mkIf cfg.enable {
-    enable = true;
-    package = pkgs.gitFull;
-    userName = usr.devName;
-    userEmail = usr.devEmail;
-    aliases = {
-      ci = "commit -m";
-      co = "checkout";
-      s = "status";
-      p = "pull";
-      ps = "push";
-      alias = "config --get-regexp ^alias";
-    };
-    extraConfig = {
-      init.defaultBranch = "main";
-      credential.helper = "${pkgs.git.override { withLibsecret = true; }}/bin/git-credential-libsecret";
-      push.autoSetupRemote = true;
-    };
-    #pkgs.pinentry-tty;
-    diff-so-fancy = {
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [ git-secrets ];
+    programs.git = {
       enable = true;
-      changeHunkIndicators = true;
-      markEmptyLines = true;
+      package = pkgs.gitFull;
+      userName = usr.devName;
+      userEmail = usr.devEmail;
+      aliases = {
+        ci = "commit -m";
+        co = "checkout";
+        s = "status";
+        p = "pull";
+        ps = "push";
+        alias = "config --get-regexp ^alias";
+      };
+      extraConfig = {
+        init.defaultBranch = "main";
+        credential.helper = "${
+            pkgs.git.override { withLibsecret = true; }
+          }/bin/git-credential-libsecret";
+        push.autoSetupRemote = true;
+      };
+      #pkgs.pinentry-tty;
+      diff-so-fancy = {
+        enable = true;
+        changeHunkIndicators = true;
+        markEmptyLines = true;
+      };
+      ignores = [ "*.swp" "node_modules" ];
     };
-    ignores = [ "*.swp" "node_modules" ];
   };
-};
 }
