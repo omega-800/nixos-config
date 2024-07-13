a7a56c700e31e41e8b0105036c30bd00913162f7
asdf <asdf>
2024-07-13 15:43:15 +0200

    asdf

--- usr/pkg/dev/git.nix
+++ usr/pkg/dev/git.nix
@@ -1,36 +1,40 @@
-{ usr, lib, config, pkgs, ... }: 
+{ usr, lib, config, pkgs, ... }:
 with lib;
-let cfg = config.u.dev;
+let cfg = config.u.dev.git;
 in {
-  config = mkIf cfg.enable {
-    home.packages = with pkgs; [
-      git-secrets
-    ];
-    programs.git = mkIf cfg.enable {
-      enable = true;
-      package = pkgs.gitFull;
-      userName = usr.devName;
-      userEmail = usr.devEmail;
-      aliases = {
-        ci = "commit -m";
-        co = "checkout";
-        s = "status";
-        p = "pull";
-        ps = "push";
-        alias = "config --get-regexp ^alias";
-      };
-      extraConfig = {
-        init.defaultBranch = "main";
-        credential.helper = "${pkgs.git.override { withLibsecret = true; }}/bin/git-credential-libsecret";
-        push.autoSetupRemote = true;
-      }; 
-#pkgs.pinentry-tty;
-      diff-so-fancy = {
-        enable = true;
-        changeHunkIndicators = true;
-        markEmptyLines = true;
-      };
-      ignores = [ "*.swp" "node_modules" ];
-    };
+  options.u.dev.git.enable = mkOption {
+    type = types.bool;
+    default = config.u.dev.git.enable;
   };
+
+  home.packages = with pkgs; [
+    git-secrets
+  ];
+  programs.git = mkIf cfg.enable {
+    enable = true;
+    package = pkgs.gitFull;
+    userName = usr.devName;
+    userEmail = usr.devEmail;
+    aliases = {
+      ci = "commit -m";
+      co = "checkout";
+      s = "status";
+      p = "pull";
+      ps = "push";
+      alias = "config --get-regexp ^alias";
+    };
+    extraConfig = {
+      init.defaultBranch = "main";
+      credential.helper = "${pkgs.git.override { withLibsecret = true; }}/bin/git-credential-libsecret";
+      push.autoSetupRemote = true;
+    };
+    #pkgs.pinentry-tty;
+    diff-so-fancy = {
+      enable = true;
+      changeHunkIndicators = true;
+      markEmptyLines = true;
+    };
+    ignores = [ "*.swp" "node_modules" ];
+  };
+};
 }
