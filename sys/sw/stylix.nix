46c6815fa3000b89214ec4ae534bfebb8cbf2d4e
omega-800 <gshevoroshkin@gmail.com>
2024-07-27 00:16:59 +0200

    will have to refactor the whole styling thing going on

--- sys/sw/stylix.nix
+++ sys/sw/stylix.nix
@@ -1,22 +1,32 @@
 { lib, pkgs, inputs, usr, ... }:
 
 let
-  themePath = "../../../themes/"+usr.theme+"/"+usr.theme+".yaml";
-  themePolarity = lib.removeSuffix "\n" (builtins.readFile (./. + "../../../themes"+("/"+usr.theme)+"/polarity.txt"));
-  myLightDMTheme = if themePolarity == "light" then "Adwaita" else "Adwaita-dark";
-  backgroundUrl = builtins.readFile (./. + "../../../themes"+("/"+usr.theme)+"/backgroundurl.txt");
-  backgroundSha256 = builtins.readFile (./. + "../../../themes/"+("/"+usr.theme)+"/backgroundsha256.txt");
+  themePath = "../../../themes/" + usr.theme + "/" + usr.theme + ".yaml";
+  themePolarity = lib.removeSuffix "\n" (builtins.readFile
+    (./. + "../../../themes" + ("/" + usr.theme) + "/polarity.txt"));
+  themeImage =
+    if builtins.pathExists (themePath + "/${usr.theme}.png") then
+      themePath + "/${usr.theme}.png"
+    else
+      pkgs.fetchurl {
+        url = builtins.readFile (themePath + "/backgroundurl.txt");
+        sha256 = builtins.readFile (themePath + "/backgroundsha256.txt");
+      };
+  myLightDMTheme =
+    if themePolarity == "light" then "Adwaita" else "Adwaita-dark";
 in
-  {
-    imports = if usr.style then [ inputs.stylix.nixosModules.stylix ] else [];
-    config = lib.mkIf usr.style {
+{
+  imports = if usr.style then [ inputs.stylix.nixosModules.stylix ] else [ ];
+  config = lib.mkIf usr.style {
     stylix = {
       autoEnable = true;
-      polarity = themePolarity;
-      image = pkgs.fetchurl {
-        url = backgroundUrl;
-        sha256 = backgroundSha256;
+      cursor = {
+        package = pkgs.bibata-cursors;
+        name = "Bibata-Modern-Ice";
+        size = 32;
       };
+      polarity = themePolarity;
+      image = themeImage;
       base16Scheme = ./. + themePath;
       fonts = {
         monospace = {
@@ -40,18 +50,26 @@ in
         lightdm.enable = true;
         console.enable = true;
         chromium.enable = true;
+        feh.enable = true;
+        gnome.enable = true;
+        gtk.enable = true;
+        kmscon.enable = true;
+        nixos-icons.enable = true;
+        nixvim.enable = true;
+        plymouth = {
+          enable = true;
+          logoAnimated = true;
+        };
         grub = {
           enable = true;
-          useImage = false;
+          useImage = true;
         };
       };
     };
-#    services.xserver.displayManager.lightdm = {
-#      greeters.slick.enable = true;
-#      greeters.slick.theme.name = myLightDMTheme;
-#    };
-    environment.sessionVariables = {
-      QT_QPA_PLATFORMTHEME = "qt5ct";
-    };
-    };
-  }
+    #    services.xserver.displayManager.lightdm = {
+    #      greeters.slick.enable = true;
+    #      greeters.slick.theme.name = myLightDMTheme;
+    #    };
+    environment.sessionVariables = { QT_QPA_PLATFORMTHEME = "qt5ct"; };
+  };
+}
