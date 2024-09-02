1ffcaaee396c8159fc3844fa89505b4e0b998e5c
omega-800 <gshevoroshkin@gmail.com>
2024-09-02 21:33:54 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- sys/sw/stylix.nix
+++ sys/sw/stylix.nix
@@ -15,61 +15,62 @@ in
   config =
     if !usr.style then
       { }
-    else {
-      stylix = {
-        autoEnable = false;
-        polarity = themePolarity;
-        opacity.terminal = 0.85;
-        image = pkgs.fetchurl {
-          url = backgroundUrl;
-          sha256 = backgroundSha256;
-        };
-        base16Scheme = ./. + themePath;
-        cursor = {
-          package = pkgs.bibata-cursors;
-          name = "Bibata-Modern-Ice";
-          size = 32;
-        };
-        fonts = {
-          monospace = {
-            name = usr.font;
-            package = usr.fontPkg;
-          };
-          serif = {
-            name = usr.font;
-            package = usr.fontPkg;
-          };
-          sansSerif = {
-            name = usr.font;
-            package = usr.fontPkg;
-          };
-          emoji = {
-            name = "Noto Color Emoji";
-            package = pkgs.noto-fonts-emoji-blob-bin;
-          };
-        };
-        targets = {
-          # lightdm.enable = true;
-          console.enable = true;
-          # feh.enable = true;
-          gnome.enable = false;
-          # gtk.enable = true;
-          # nixos-icons.enable = true;
-          chromium.enable = false;
-          nixvim.enable = true;
-          grub = {
-            enable = true;
-            # useImage = true;
-          };
-          # plymouth = {
-          #   enable = true;
-          #   logo = pkgs.fetchurl {
-          #     url = backgroundUrl;
-          #     sha256 = backgroundSha256;
-          #   };
-          #   logoAnimated = true;
-          # };
-        };
+    else
+      {
+        # stylix = {
+        #   autoEnable = false;
+        #   polarity = themePolarity;
+        #   opacity.terminal = 0.85;
+        #   image = pkgs.fetchurl {
+        #     url = backgroundUrl;
+        #     sha256 = backgroundSha256;
+        #   };
+        #   base16Scheme = ./. + themePath;
+        #   cursor = {
+        #     package = pkgs.bibata-cursors;
+        #     name = "Bibata-Modern-Ice";
+        #     size = 32;
+        #   };
+        #   fonts = {
+        #     monospace = {
+        #       name = usr.font;
+        #       package = usr.fontPkg;
+        #     };
+        #     serif = {
+        #       name = usr.font;
+        #       package = usr.fontPkg;
+        #     };
+        #     sansSerif = {
+        #       name = usr.font;
+        #       package = usr.fontPkg;
+        #     };
+        #     emoji = {
+        #       name = "Noto Color Emoji";
+        #       package = pkgs.noto-fonts-emoji-blob-bin;
+        #     };
+        #   };
+        #   targets = {
+        #     # lightdm.enable = true;
+        #     console.enable = true;
+        #     # feh.enable = true;
+        #     gnome.enable = false;
+        #     # gtk.enable = true;
+        #     # nixos-icons.enable = true;
+        #     chromium.enable = false;
+        #     nixvim.enable = true;
+        #     grub = {
+        #       enable = true;
+        #       # useImage = true;
+        #     };
+        #     # plymouth = {
+        #     #   enable = true;
+        #     #   logo = pkgs.fetchurl {
+        #     #     url = backgroundUrl;
+        #     #     sha256 = backgroundSha256;
+        #     #   };
+        #     #   logoAnimated = true;
+        #     # };
+        #   };
+        # };
       };
-    };
 }
