1d645fe00cc217471588699aeed3d924ef827dcf
omega-800 <gshevoroshkin@gmail.com>
2024-10-05 15:35:25 +0200

    pass

--- modules/hosts/nixie-dev/configuration.nix
+++ modules/hosts/nixie-dev/configuration.nix
@@ -6,7 +6,6 @@
 
   m.os.boot.mode = "bios";
   m.dev.virt.enable = false;
-  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
   boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
   system.stateVersion = "24.05";
