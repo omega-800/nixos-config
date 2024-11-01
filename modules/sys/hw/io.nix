5df6c315ecca7af8d3f9a044b0d234b75a31d673
omega-800 <gshevoroshkin@gmail.com>
2024-11-01 19:39:09 +0100

    fixed kanata binding interference with tmux

--- modules/sys/hw/io.nix
+++ modules/sys/hw/io.nix
@@ -51,87 +51,86 @@ in
               )
             '';
         };
-      };
-      keyboards.main = {
-        devices = [
-          "/dev/input/by-path/pci-0000:00:1f.3-platform-skl_hda_dsp_generic-event"
-          "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
-          "/dev/input/by-path/platform-INT33D5:00-event"
-        ];
+        keyboards.main = {
+          devices = [
+            "/dev/input/by-path/pci-0000:00:1f.3-platform-skl_hda_dsp_generic-event"
+            "/dev/input/by-path/platform-i8042-serio-0-event-kbd"
+            "/dev/input/by-path/platform-INT33D5:00-event"
+          ];
 
-        extraDefCfg = "process-unmapped-keys yes";
-        config = # lisp
-          ''
-            (defsrc
-              caps bspc u i a s d f g h j k l ;
-            )
-            (defvar
-              tap-time 150
-              hold-time 200
-            )
-            (deflayermap (base)
-              ${
-                optionalString sc ''
-                  caps bspc 
-                  bspc caps
-                ''
-              }
-              ${
-                optionalString hm ''
-                  a (tap-hold $tap-time $hold-time a lalt)
-                  s (tap-hold $tap-time $hold-time s lmet)
-                  d (tap-hold $tap-time $hold-time d lsft)
-                  f (tap-hold $tap-time $hold-time f lctl)
-                  j (tap-hold $tap-time $hold-time j rctl)
-                  k (tap-hold $tap-time $hold-time k rsft)
-                  l (tap-hold $tap-time $hold-time l rmet)
-                  ; (tap-hold $tap-time $hold-time ; ralt)
-                ''
-              }
-              ${
-                optionalString vl ''
-                  lsft esc
-                  rsft (layer-switch vim)
-                  spc (tap-hold $tap-time $hold-time spc (layer-while-held vim))
-                ''
-              }
-            )
-            ${optionalString vl ''
-              (deflayermap (vim)
+          extraDefCfg = "process-unmapped-keys yes";
+          config = # lisp
+            ''
+              (defsrc
+                caps bspc u i a s d f g h j k l ;
+              )
+              (defvar
+                tap-time 150
+                hold-time 200
+              )
+              (deflayermap (base)
                 ${
                   optionalString sc ''
                     caps bspc 
                     bspc caps
                   ''
                 }
-                lsft esc
-                rsft (layer-switch base)
-                spc (tap-hold $tap-time $hold-time spc (layer-while-held base))
-                h left 
-                j down 
-                k up 
-                l right
-                g tab 
-                u pgup 
-                d pgdn 
-                a end 
-                i home
-                f esc
-                s lsft
+                ${
+                  optionalString hm ''
+                    a (tap-hold $tap-time $hold-time a lalt)
+                    s (tap-hold $tap-time $hold-time s lmet)
+                    d (tap-hold $tap-time $hold-time d lsft)
+                    f (tap-hold $tap-time $hold-time f lctl)
+                    j (tap-hold $tap-time $hold-time j rctl)
+                    k (tap-hold $tap-time $hold-time k rsft)
+                    l (tap-hold $tap-time $hold-time l rmet)
+                    ; (tap-hold $tap-time $hold-time ; ralt)
+                  ''
+                }
+                ${
+                  optionalString vl ''
+                    lsft esc
+                    rsft (layer-switch vim)
+                    lctl (tap-hold $tap-time $hold-time spc (layer-while-held vim))
+                  ''
+                }
               )
-            ''}
-          '';
+              ${optionalString vl ''
+                (deflayermap (vim)
+                  ${
+                    optionalString sc ''
+                      caps bspc 
+                      bspc caps
+                    ''
+                  }
+                  lsft esc
+                  rsft (layer-switch base)
+                  spc (tap-hold $tap-time $hold-time spc (layer-while-held base))
+                  h left 
+                  j down 
+                  k up 
+                  l right
+                  g tab 
+                  u pgup 
+                  d pgdn 
+                  a end 
+                  i home
+                  f esc
+                  s lsft
+                )
+              ''}
+            '';
+        };
       };
-    };
-    libinput = {
-      # only enable if config.services.xserver.enable
-      # enable = true;
-      touchpad = mkIf cfg.touchpad.enable {
-        tapping = true;
-        middleEmulation = true;
-        disableWhileTyping = true;
+      libinput = {
+        # only enable if config.services.xserver.enable
+        # enable = true;
+        touchpad = mkIf cfg.touchpad.enable {
+          tapping = true;
+          middleEmulation = true;
+          disableWhileTyping = true;
+        };
       };
     };
   };
-};
 }
