bbf0d509e3b6080c1953f8f4b9cb3bb508d6ab9f
Your Name <you@example.com>
2024-11-03 19:13:05 +0100

    added srv modules

---
+++ modules/sys/srv/default.nix
@@ -0,0 +1,15 @@
+{
+  imports = [
+    ./acme.nix
+    ./attic.nix
+    ./elasticsearch.nix
+    ./firefly.nix
+    ./gitlab.nix
+    ./grafana.nix
+    ./http.nix
+    ./keycloak.nix
+    ./nextcloud.nix
+    ./prometheus.nix
+    ./wiki-js.nix
+  ];
+}
