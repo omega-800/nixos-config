7cc8117fece1a7e3455820b5923dc71713950f45
omega-800 <gshevoroshkin@gmail.com>
2025-09-10 12:41:24 +0200

    gojira

---
+++ src/configs/nodes/gojira/omega.nix
@@ -0,0 +1,31 @@
+{ lib, pkgs, ... }:
+{
+  config.c = {
+    net = {
+      id = 123;
+      network = [
+        10
+        0
+        0
+      ];
+    };
+    sys = {
+      profile = "serv";
+      system = "x86_64-linux";
+      genericLinux = false;
+      paranoid = lib.mkForce false;
+      flavors = [
+        "child"
+        "hoster"
+      ];
+    };
+    usr = {
+      shell = pkgs.zsh;
+      theme = "gruvbox-dark-hard";
+      termColors = {
+        c1 = "35";
+        c2 = "90";
+      };
+    };
+  };
+}
