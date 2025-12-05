07b6681b3a02ce4cf1a6aabe843a6a329a48c01b
omega-800 <gshevoroshkin@gmail.com>
2025-12-06 00:30:50 +0100

    ssh

--- src/configs/nodes/fugazi/omega.nix
+++ src/configs/nodes/fugazi/omega.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }:
 {
   config.c = {
+    net.pubkeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFAWLgOdIuSkQ/oqgCS424xPL1rHXH88/EtWk/bYuw9e omega@fugazi" ];
     sys = {
       profile = "pers";
       system = "x86_64-linux";
