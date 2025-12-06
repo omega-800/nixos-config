821cc6fdc259905caa11c1e35caf2048b2ac69f7
omega-800 <gshevoroshkin@gmail.com>
2025-12-06 19:30:57 +0100

    eh

--- src/configs/nodes/fugazi/nixos.nix
+++ src/configs/nodes/fugazi/nixos.nix
@@ -7,7 +7,7 @@
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware = {
     nvidia = {
-      open = true;
+      open = false;
       nvidiaSettings = true;
       videoAcceleration = true;
       modesetting.enable = true;
